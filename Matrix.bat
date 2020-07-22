f0495927efa1b7fa662cc4c46bb4285f84688837
Benjamin Napier <31937804+napierbw@users.noreply.github.com>
2020-07-22 17:40:20 -0400

    Add files via upload

---
+++ Matrix.bat
@@ -0,0 +1,18 @@
+@echo off
+title Matrix
+color 0a
+mode 1000
+
+goto Greeting
+
+:Greeting
+cls
+echo Hello Neo.
+pause
+cls
+goto Matrix
+
+:Matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+PING 1.1.1.1 -n 1 -w 0.3 >NUL
+goto Matrix
